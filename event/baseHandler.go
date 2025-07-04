fa284da0718a1070f1f63a8e96ab75c2f80fd00b
craftman2868 <57369130+Craftman2868@users.noreply.github.com>
2025-07-05 00:55:38 +0200

    event: Bind(), automatically bind a function to its event

--- event/baseHandler.go
+++ event/baseHandler.go
@@ -18,5 +18,20 @@ func (h *BaseHandler) On(eventName string, handler func(Event)) {
 	if h.handlers == nil {
 		h.handlers = make(map[string][]func(Event), 1)
 	}
+
 	h.handlers[eventName] = append(h.handlers[eventName], handler)
+}
+
+type onable interface {
+	On(string, func(Event))
+}
+
+// Automatically bind the function to the matching event
+// WARNING: MUST NOT be used with an interface as E (e.g. Event), E must be a struct that implement Event
+func Bind[E Event](h onable, handler func(E)) {
+	var proto E
+
+	h.On(proto.Name(), func(ev Event) {
+		handler(ev.(E))
+	})
 }
